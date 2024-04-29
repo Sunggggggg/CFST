c3a7bbbf84789be2cfe05c5d86f6523592fb0389
Sunggggggg <sky990319@gmail.com>
2024-04-29 14:22:20 +0900

    f

--- lib/core/trainer.py
+++ lib/core/trainer.py
@@ -185,9 +185,7 @@ class Trainer():
             total_loss = gen_loss
             if self.device > 0 :
                 total_loss, total_instance = self.sync_data(total_loss)
-                self.loss_meter_update(losses, total_loss.item(), loss_dict, total_instance)
-            else :
-                losses = total_loss
+            self.loss_meter_update(losses, total_loss.item(), loss_dict, total_instance)
 
             timer['backward'] = time.time() - start
             timer['batch'] = timer['data'] + timer['forward'] + timer['loss'] + timer['backward']
