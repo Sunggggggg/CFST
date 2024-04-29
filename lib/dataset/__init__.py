ae82463f72832e36d7c19b329519363cfa511908
Sunggggggg <sky990319@gmail.com>
2024-04-29 13:17:19 +0900

    f

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -1,4 +1,7 @@
-#from ._dataset_2d import Dataset2D
+#from .dataset_2d import Dataset2D
 from .dataset_3d import Dataset3D
 
+from .insta import Insta
+from .mpii3d import MPII3D
 from .threedpw import ThreeDPW
+from .h36m import Human36M
